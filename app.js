4b887d340f9d1330e792eac62afe743d33a85114
unow30 <yunho7205@gmail.com>
2021-01-14 00:34:12 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//js에서 유효한 코드는 ts에서도 유효
+//ts는 js로 컴파일되어 실행된다.
+function logName(name) {
+    console.log(name);
+}
+logName('its me');
---
+++ app.ts
@@ -0,0 +1,7 @@
+//js에서 유효한 코드는 ts에서도 유효
+//ts는 js로 컴파일되어 실행된다.
+function logName(name: string) {
+    console.log(name);
+}
+
+logName('its me');
